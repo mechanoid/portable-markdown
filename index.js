698eb7f1056b7edaad50dd2d312d0865320e6750
Falk Hoppe <falkhoppe81@gmail.com>
2017-02-25 14:47:28 +0100

    writing md directly to pdf

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const path = require('path')
 
 const md2Html = require(path.resolve(__dirname, 'lib', 'md-2-html'))
+const html2Pdf = require(path.resolve(__dirname, 'lib', 'html-2-pdf'))
 
 module.exports = (config) =>
   md2Html(config)
-    .then(content => console.log(content))
+    .then(content => html2Pdf(content, config))
     .then(() => console.log('finished'))
     .catch(e => {
       if (e.name === 'PortableMarkdownError') {
         console.error('PortableMarkdownError', '::', e.message)
         return
       }
-
+      console.log(e)
       throw e
     })
