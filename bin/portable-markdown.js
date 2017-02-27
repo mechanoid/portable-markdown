d01d761f7ac7c03a5cdb6864c530590409a3f500
Falk Hoppe <falkhoppe81@gmail.com>
2017-02-27 09:18:05 +0100

    obtain options directly from package file instead of env tunnel

--- bin/portable-markdown.js
+++ bin/portable-markdown.js
@@ -2,9 +2,14 @@
 
 const path = require('path')
 
+const cwd = process.cwd()
 const portableMarkdown = require(path.resolve(__dirname, '..', 'index'))
+
+const pckg = require(path.resolve(cwd, 'package.json'))
+
+const options = pckg.config && pckg.config.pmd && pckg.config.pmd.options
 
 portableMarkdown({
   entry: process.env.npm_package_config_pmd_entry,
   target: process.env.npm_package_config_pmd_target
-}, process.env.npm_package_config_pmd_options || {})
+}, options || {})
